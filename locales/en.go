14e4d00514a5f8ac9ff8ed7909cad70fc1bfc7de
Your Name <you@example.com>
2018-10-22 06:27:07 -0300

    Start of localization code

---
+++ locales/en.go
@@ -0,0 +1,5 @@
+package localization
+
+var en_US = map[string]string{
+	"help_usage": "something something",
+}
---
+++ locales/localization.go
@@ -0,0 +1,3 @@
+package localization
+
+// TODO: Load default locale from yml and make available to cli functions
