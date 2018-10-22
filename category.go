58652ba3b2bc72ca949e42d0b3c9b2ce5b8ccac9
Your Name <you@example.com>
2018-10-22 20:48:30 -0300

    Reducing usage of pointers since they are not threadsafe, not really necessary in many places being used

--- category.go
+++ category.go
@@ -1,31 +1,15 @@
 package cli
 
-// CommandCategories is a slice of *CommandCategory.
-type CommandCategories []*CommandCategory
+type CommandCategories []CommandCategory
 
-// CommandCategory is a category containing commands.
 type CommandCategory struct {
 	Name     string
+	Hidden   bool
 	Commands Commands
 }
 
-// TODO: How necessary is this? ARe we often softing by name for categories?
-// can we not just do ordered insertion?
-func (self CommandCategories) Less(i, j int) bool {
-	return lexicographicLess(self[i].Name, self[j].Name)
-}
-
-func (self CommandCategories) Len() int {
-	return len(self)
-}
-
-func (self CommandCategories) Swap(i, j int) {
-	self[i], self[j] = self[j], self[i]
-}
-
-// AddCommand adds a command to a category.
 func (self CommandCategories) AddCommand(category string, command Command) CommandCategories {
-	for _, commandCategory := range self {
+	for _, category := range self {
 		if commandCategory.Name == category {
 			commandCategory.Commands = append(commandCategory.Commands, command)
 			return self
@@ -34,8 +18,7 @@ func (self CommandCategories) AddCommand(category string, command Command) Comma
 	return append(self, &CommandCategory{Name: category, Commands: []Command{command}})
 }
 
-// VisibleCommands returns a slice of the Commands with Hidden=false
-func (self *CommandCategory) VisibleCommands() []Command {
+func (self CommandCategory) VisibleCommands() []Command {
 	ret := []Command{}
 	for _, command := range self.Commands {
 		if !command.Hidden {
