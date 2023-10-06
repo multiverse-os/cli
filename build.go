ea6fb6c5e334aeda82f6a0eaacfab002758e318b
Your Name Here <you@example.com>
2023-10-06 06:43:23 -0700

    very frustrating because I had CLI working which is why I moved onto webframework again

--- build.go
+++ build.go
@@ -1,32 +1,28 @@
 package cli
 
-import (
-	"time"
-)
-
-type Build struct {
-	CompiledAt time.Time
-	Source     string
-	Commit     string
-	Signature  string
-	Developers []Developer
-}
-
-type Developer struct {
-	PublicKey PublicKey
-	Name      string
-	Email     string
-}
-
-type KeyType int
-
-const (
-	RSA KeyType = iota
-	ECDSA
-	ED25199
-)
-
-type PublicKey struct {
-	Type      KeyType
-	PublicKey string
-}
+//type Build struct {
+//	CompiledAt time.Time
+//	Source     string
+//	Commit     string
+//	Signature  string
+//	Developers []Developer
+//}
+//
+//type Developer struct {
+//	PublicKey PublicKey
+//	Name      string
+//	Email     string
+//}
+//
+//type KeyType int
+//
+//const (
+//	RSA KeyType = iota
+//	ECDSA
+//	ED25199
+//)
+//
+//type PublicKey struct {
+//	Type      KeyType
+//	PublicKey string
+//}
