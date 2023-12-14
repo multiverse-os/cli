2cc527fe7cdf2750e9be3da5c3b8761a8dcf43fe
Your Name <you@example.com>
2023-12-14 08:59:55 -0500

    i just cant figure out why this indirect is included since other packages like ansi follow same rules

--- go.mod
+++ go.mod
@@ -9,10 +9,8 @@ require (
 	github.com/multiverse-os/cli/terminal/text/banner v0.1.0
 )
 
-require (
-	github.com/multiverse-os/banner v0.1.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-)
+//github.com/multiverse-os/banner v0.1.0 // indirect
+require golang.org/x/text v0.14.0 // indirect
 
 replace (
 	github.com/multiverse-os/cli/data => ./data
--- process.go
+++ process.go
@@ -1,21 +1,21 @@
 package cli
 
 import (
+	"os"
 	"path/filepath"
-  "os"
 )
 
 type process struct {
-  ID          int
-  CWD         string
-  Executable  string
+	ID         int
+	CWD        string
+	Executable string
 }
 
 func Process() process {
 	cwd, executable := filepath.Split(os.Args[0])
-  return process{
-    ID: os.Getpid(),
-    CWD: cwd,
-    Executable: executable,
-  }
+	return process{
+		ID:         os.Getpid(),
+		CWD:        cwd,
+		Executable: executable,
+	}
 }
